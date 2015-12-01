e1eb735f7dbe05fe23a431f4eb437940a83662c9
Vidare90 <vidare90@gmail.com>
2015-12-01 14:27:45 +0000

    Preparing find function

--- Nerd/main.cpp
+++ Nerd/main.cpp
@@ -109,16 +109,6 @@ void addNerd(int &number, vector<Nerd> &ComputerScientists)
 
 }
 
-void pickOptions(int &number)
-{
-    cout << "Welcome to the Ultimade guide of Computer Scientists!" << endl;
-    cout << "-----------------------------------------------------" << endl;
-    cout << "\t1. Show a list of every computer scientists" << endl;
-    cout << "\t2. Add computer scientist to the list" << endl;
-    cout << "\tPlease select a number: ";
-    cin >> number;
-}
-
 void makeHeader()
 {
     cout << "Welcome to the Ultimade guide of Computer Scientists!" << endl;
@@ -129,6 +119,7 @@ void pickOptions(int &number, vector<Nerd> &ComputerScientists)
 {
     cout << "\t1. Show a list of every computer scientists" << endl;
     cout << "\t2. Add computer scientist to the list" << endl;
+    cout << "\t3. Search for scientists in the list" << endl;
     cout << "\t9. Quit program" << endl;
     cout << endl;
     cout << "Please select a number: ";
@@ -152,6 +143,9 @@ void checkOptions(int &number, vector<Nerd> &ComputerScientists)
         case 2:
             system("CLS");
             addNerd(number, ComputerScientists);
+            break;
+        case 3:
+            system("CLS");
             break;
         case 9:
             cout << "Program shut down" << endl;
